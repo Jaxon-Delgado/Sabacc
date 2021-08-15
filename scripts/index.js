3997fde8329d790fa734261859754619b4eece62
cbartal <cbartal@vt.edu>
2021-08-14 23:09:06 -0400

    added transition to hyperspace baby

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+
+
+function clearTitle() {
+    console.log("Clicked these nuts");
+    //document.getElementById("titleDiv").hidden = true;
+    fade(document.getElementById("titleDiv"));
+}
+
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
